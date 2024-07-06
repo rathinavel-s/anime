961fa1d5e3b57c011bb9302e2b4ea96911ea2883
rathinavel-s <raguls5460@gmail.com>
2024-07-06 17:50:52 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import{useEffect, useState } from 'react';
+import Fetchs from './components/Fetchs';
+
+
+
+
 
 function App() {
+ 
+const [images,setimage] =useState([])
+  const API_URL ="https://api.jikan.moe/v4/anime?q=naruto&limit=20"
+
+useEffect(()=>{
+   const  fetchPhoto = async()=>{
+     try{
+      const Response =await fetch(API_URL)
+      console.log(Response)
+      if(!Response.ok) throw Error ("fetch Error")
+        const data = await Response.json()
+      setimage(data.data)
+     }catch (err){
+      console.log(err.message)
+     }
+   }
+    fetchPhoto()
+},[])
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Fetchs
+      images={images}
+      setimages={setimage}  />
     </div>
   );
 }
