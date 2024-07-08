7871cc612ac7b9d645f8502b8b2a359bb25489d6
rathinavel-s <131685386+rathinavel-s@users.noreply.github.com>
2024-07-08 12:59:57 +0530

    Update Fetchs.js

--- src/components/Fetchs.js
+++ src/components/Fetchs.js
@@ -6,7 +6,9 @@ const Fetchs = ({images,setimage}) => {
        {images.map((image) => (
         <div className="image-card" key={image.mal_id}>
           <h3>{image.title.length ?image.title.slice(0,19) :image.title}</h3>
-          <img src={image.images.jpg.image_url} alt={image.title} />
+          <a href={image.url} target='_blank' rel='noopener noreferrer'>
+            <img src={image.images.jpg.image_url} alt={image.title} />
+          </a>
         </div>
       ))}
       </div>
