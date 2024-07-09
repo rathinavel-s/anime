4581dce575ae5aa1a995d7bd6ad111f3648af833
rathinavel-s <131685386+rathinavel-s@users.noreply.github.com>
2024-07-09 11:17:45 +0530

    Update Fetchs.js

--- src/components/Fetchs.js
+++ src/components/Fetchs.js
@@ -1,11 +1,27 @@
 import React from 'react'
-
+import { useEffect,useState } from 'react';
 const Fetchs = ({images,setimage}) => {
+   const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsSmallScreen(window.innerWidth <= 576);
+    };
+
+    checkScreenSize();
+    window.addEventListener('resize', checkScreenSize);
+
+    return () => window.removeEventListener('resize', checkScreenSize);
+  }, []);
+  
+  const truncateTitle = (title) => {
+    return title.length > 19 ? title.slice(0, 19) + '...' : title;
+  };
   return (
     <div className="image-container"> 
        {images.map((image) => (
         <div className="image-card" key={image.mal_id}>
-          <h3>{image.title.length ?image.title.slice(0,19) :image.title}</h3>
+         <h3>{isSmallScreen ? image.title : truncateTitle(image.title)}</h3>
           <a href={image.url} target='_blank' rel='noopener noreferrer'>
             <img src={image.images.jpg.image_url} alt={image.title} />
           </a>
